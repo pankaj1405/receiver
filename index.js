7737a566395390dba514e986b16bb4b0231f0979
pankaj1405 <98148058+pankaj1405@users.noreply.github.com>
2023-12-12 17:40:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 3000;
 
 // Azure AD configuration for receiver
 const azureAdConfig = {
