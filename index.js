d5f3fa7f869b82c11914268e584edac96376060b
pankaj1405 <98148058+pankaj1405@users.noreply.github.com>
2023-12-12 22:52:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ async function validateAccessToken(req, res, next) {
     const authorizationHeader = req.headers['authorization'];
 
     if (!authorizationHeader) {
-        return res.status(401).json({ error: 'Unauthorized: Access token missing' });
+        return res.status(402).json({ error: 'Unauthorized: Access token missing' });
     }
 
     const accessToken = authorizationHeader.split(' ')[1];
