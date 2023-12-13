321d4cb05520398e0f13c78fb6742afd41c25387
pankaj1405 <98148058+pankaj1405@users.noreply.github.com>
2023-12-13 14:39:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,24 @@
 const express = require('express');
-const axios = require('axios');
 const jwt = require('jsonwebtoken');
 
+// Replace with your Azure AD application details
+const audience = '6e0063e1-5b74-4730-99c6-2b1541144e5c';
+
 const app = express();
-const port = process.env.PORT || 3000;
 
-// Azure AD configuration for receiver
-const azureAdConfig = {
-    tenantId: 'add67cd2-c8b2-416c-b171-b61b22be92f4',
-    clientId: '6e0063e1-5b74-4730-99c6-2b1541144e5c',
-    clientSecret: 'NJR8Q~KkS4-EIPKtJMEIS5EeHLlCNfBAzH2fmaFc',
-};
+app.post('/', (req, res) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, audience);
 
-async function validateAccessToken(req, res, next) {
-    const authorizationHeader = req.headers['authorization'];
+    console.log(`Received message from sender: ${req.body.message}`);
+    console.log(`Decoded token claims:`, decodedToken);
 
-    if (!authorizationHeader) {
-        return res.status(401).json({ error: 'Unauthorized: Access token missing' });
-    }
-
-    const accessToken = authorizationHeader.split(' ')[1];
-
-    try {
-        // Verify the access token using the JWT library and Azure AD public keys
-        const decoded = jwt.verify(accessToken, azureAdConfig.clientSecret);
-
-        // If token is valid, proceed to the next middleware
-        next();
-    } catch (error) {
-        console.error('Error validating access token:', error.message);
-        res.status(401).json({ error: 'Unauthorized: Invalid access token' });
-    }
-}
-
-// Default Response on home
-app.get('/', (req, res) => {
-  res.send('Hello, World! receiver.....');
+    res.send('Message received and processed successfully!');
+  } catch (error) {
+    console.error(error);
+    res.status(401).send('Invalid token');
+  }
 });
 
-// Handle requests to receive data
-app.get('/api/data', validateAccessToken, (req, res) => {
-    res.json({ message: 'Data from Receiver App' });
-});
-
-// Start the receiver app
-app.listen(port, () => {
-    console.log(`Receiver app listening at http://localhost:${port}`);
-});
+app.listen(4000, () => console.log('Receiver listening on port 4000'));
